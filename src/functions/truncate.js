35089b05b3d61dd5e6d562e64754bb6c6a4dd6b7
AlexisLanderos <aj.landeros24@info.uas.edu.mx>
2026-05-28 10:43:18 -0700

    add truncate file

---
+++ src/functions/truncate.js
@@ -0,0 +1,11 @@
+function truncate(text, length) {
+  if (!text) return "";
+  
+  if (text.length <= length) {
+    return text;
+  }
+
+  return text.substring(0, length) + "...";
+}
+
+module.exports = truncate;
